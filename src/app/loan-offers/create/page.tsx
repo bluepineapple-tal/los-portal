601172ccb1af2c96607d8226dcba363fe3a4ab95
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-02-17 13:51:32 +0530

    feat: add page components

---
+++ src/app/loan-offers/create/page.tsx
@@ -0,0 +1,9 @@
+export default function CreateLoanOffer() {
+  return (
+    <div>
+      <h1>Create Loan Offer</h1>
+      <div className="h-screen"></div>
+      <div className="h-screen"></div>
+    </div>
+  );
+}
---
+++ src/app/loan-offers/page.tsx
@@ -0,0 +1,9 @@
+export default function LoanOffers() {
+  return (
+    <div>
+      <h1>Loan Offers</h1>
+      <div className="h-screen"></div>
+      <div className="h-screen"></div>
+    </div>
+  );
+}
