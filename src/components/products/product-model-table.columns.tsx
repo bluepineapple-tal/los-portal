649f59692951846e1e7c5d7ae3cc9b90c28e0b01
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-03-20 12:01:01 +0530

    fix: model data table row actions

--- src/components/products/product-model-table.columns.tsx
+++ src/components/products/product-model-table.columns.tsx
@@ -13,10 +13,22 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { toast } from "@/hooks/use-toast";
+import { deleteProductModel } from "@/lib/functions/product-model.api";
 import { ColumnDef } from "@tanstack/react-table";
 
 import { Badge } from "../ui/badge";
 import { DataTableColumnHeader } from "../ui/data-table-column-header";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
 import { IProductModel, ProductStatus } from "./product.interface";
 
 export const productModelTableColumns: ColumnDef<IProductModel>[] = [
@@ -46,7 +58,7 @@ export const productModelTableColumns: ColumnDef<IProductModel>[] = [
   {
     accessorKey: "name",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Name" />
+      <DataTableColumnHeader column={column} title="Model" />
     ),
   },
   {
@@ -100,29 +112,72 @@ export const productModelTableColumns: ColumnDef<IProductModel>[] = [
     id: "actions",
     enableHiding: false,
     cell: ({ row }) => {
-      const product = row.original;
+      const productModel = row.original;
 
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(product.id)}
-            >
-              Copy product ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Link href={`/products/${product.id}`}>View product details</Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Dialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => navigator.clipboard.writeText(productModel.id)}
+              >
+                Copy product model ID
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href={`/products/${productModel.slug}`}>
+                  View model details
+                </Link>
+              </DropdownMenuItem>
+
+              <DropdownMenuSeparator />
+
+              <DialogTrigger asChild>
+                <DropdownMenuItem>
+                  <Button variant={"destructive"} className="flex-1">
+                    Delete
+                  </Button>
+                </DropdownMenuItem>
+              </DialogTrigger>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                <span className="text-destructive">
+                  This action cannot be undone. This will permanently delete{" "}
+                  <strong>{productModel.name}.</strong>
+                </span>
+              </DialogDescription>
+              <DialogFooter className="sm:justify-start">
+                <DialogClose asChild>
+                  <Button
+                    variant={"destructive"}
+                    className="flex-1"
+                    onClick={async () => {
+                      const success = await deleteProductModel(productModel.id);
+                      if (success) {
+                        toast({
+                          title: `Successfully deleted ${productModel.name}`,
+                        });
+                      }
+                    }}
+                  >
+                    Delete {productModel.name}
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       );
     },
   },
