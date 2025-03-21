d512065acd3e3e959605718e03cc927f6eaf9d7c
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-03-21 10:00:03 +0530

    fix: staging env issues

--- src/config/appInfo.ts
+++ src/config/appInfo.ts
@@ -1,8 +1,8 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
-  appName: "LOS",
-  apiDomain: "http://localhost:8000",
-  websiteDomain: "http://localhost:3000",
+  appName: process.env.APP_NAME ?? "LOS",
+  apiDomain: process.env.API_DOMAIN ?? "http://localhost:8000",
+  websiteDomain: process.env.WEBSITE_DOMAIN ?? "http://localhost:3000",
   apiBasePath: "/auth",
   websiteBasePath: "/auth",
 };
