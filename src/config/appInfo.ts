91f1167bdbd3fbde57f76a4a7b9676f7c17677e3
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-03-21 10:18:16 +0530

    fix: append the env with next public

--- src/config/appInfo.ts
+++ src/config/appInfo.ts
@@ -1,8 +1,9 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
-  appName: process.env.APP_NAME ?? "LOS",
-  apiDomain: process.env.API_DOMAIN ?? "http://localhost:8000",
-  websiteDomain: process.env.WEBSITE_DOMAIN ?? "http://localhost:3000",
+  appName: process.env.NEXT_PUBLIC_APP_NAME ?? "LOS",
+  apiDomain: process.env.NEXT_PUBLIC_API_DOMAIN ?? "http://localhost:8000",
+  websiteDomain:
+    process.env.NEXT_PUBLIC_WEBSITE_DOMAIN ?? "http://localhost:3000",
   apiBasePath: "/auth",
   websiteBasePath: "/auth",
 };
